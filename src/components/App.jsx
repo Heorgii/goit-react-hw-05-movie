855af4ebd01f1fda6e4b9d32dc47a87659cf80f4
Heorgii <rushchakheorgii@gmail.com>
2022-11-30 19:40:04 +0300

    some fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 // import Loader from "./Loader/Loader";
 // lazy
 // import { Suspense } from "react";
-import { Route } from 'react-router-dom';
-// import MovieReviews from "page/Reviews/Reviews";
-// import MovieCast from "page/Cast/Cast";
+import { Route, Routes } from 'react-router-dom';
+import MovieReviews from "page/Reviews/Reviews";
+import MovieCast from "page/Cast/Cast";
 import AppBar from "./SharedLayout/SharedLayout";
 import Home from "page/Home/Home";
-// import Movies from "page/Movies/Movies";
-// import MovieDetails from "page/MovieDetails/MovieDetails";
+import Movies from "page/Movies/Movies";
+import MovieDetails from "page/MovieDetails/MovieDetails";
 // import Navigation from "./Navigation/Navigation";
 
 import 'styles.css';
@@ -29,17 +29,17 @@ export const App = () => {
       {/* <Suspense fallback={<Loader />}> */}
 
       {/* <Navigation /> */}
-      {/* <Routes> */}
-      <Route path="/" element={<AppBar />} >
-        <Route index element={<Home />} />
-        {/* <Route path="/movies" element={<Movies />} />
-            <Route path="/movies/:movieId" element={<MovieDetails />}>
-              <Route path="reiwers" element={<MovieReviews />} />
-              <Route path="cast" element={<MovieCast />} />
-            </Route> */}
-      </Route>
-      {/* <Route path="*" element={<Home />} /> */}
-      {/* </Routes> */}
+      <Routes>
+        <Route path="/" element={<AppBar />} >
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="reiwers" element={<MovieReviews />} />
+            <Route path="cast" element={<MovieCast />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Routes>
 
       {/* </Suspense> */}
 
